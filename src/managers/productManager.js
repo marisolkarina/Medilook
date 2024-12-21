bcfca61077ecdc206168cf92fd4542d0314fbcbb
marisolkarina <marisol.kari.20@gmail.com>
2024-12-21 16:53:51 -0500

    get producto por id, delete, update

--- src/managers/productManager.js
+++ src/managers/productManager.js
@@ -1,24 +1,77 @@
 import fs from "node:fs";
-// import { v4 as uuidv4 } from "uuid";
-import path from 'path'
+import crypto from 'crypto';
+import path from 'path';
 
 class ProductManager {
     constructor(path) {
         this.path = path;
-
-        if (!fs.existsSync(this.path)) {
-            fs.writeFileSync(this.path, JSON.stringify([]), 'utf8');
-        }
     }
 
-    async obtenerProductos() {
+    async getProducts() {
         try {
-            const productos = await fs.promises.readFile(this.path, "utf-8");
-            return JSON.parse(productos);
-        } catch (error) {
-            throw new Error(error.message);
+            if (fs.existsSync(this.path)) {
+                const productos = await fs.promises.readFile(this.path, "utf-8");
+                return JSON.parse(productos);
+              } else return [];
+        } catch (err) {
+            throw new Error(err.message);
         }
     }
+
+    async getProductById(id) {
+        try {
+            const productos = await this.getProducts();
+            const productoBuscado = productos.find((p) => p.id === id);
+            if (!productoBuscado) throw new Error("El producto no existe");
+            return productoBuscado;
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
+    async addProduct(producto) {
+        try {
+            const newProduct = {
+                id: crypto.randomBytes(10).toString('hex'),
+                ...producto
+            };
+            const productos = await this.getProducts();
+            productos.push(newProduct);
+            await fs.promises.writeFile(this.path, JSON.stringify(productos));
+            return newProduct;
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
+    async update(producto, id) {
+        try {
+            const productos = await this.getProducts();
+            let productoActualizar = await this.getProductById(id);
+            productoActualizar = { ...productoActualizar, ...producto };
+
+            const productosActualizado = productos.filter((prod) => prod.id !== id);
+            productosActualizado.push(productoActualizar);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(productosActualizado));
+            return productoActualizar;
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
+    async delete(id) {
+        try {
+            const productoEliminar = await this.getProductById(id);
+            const productos = await this.getProducts();
+            const productosActualizado = productos.filter((prod) => prod.id !== id);
+            await fs.promises.writeFile(this.path, JSON.stringify(productosActualizado));
+            return productoEliminar;
+        } catch (err) {
+            throw new Error(err.message);
+        }
+    }
+
 
 }
 
--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -5,13 +5,52 @@ const router = Router();
 
 router.get("/", async (req, res) => {
     try {
-        const productos = await productManager.obtenerProductos();
+        const productos = await productManager.getProducts();
         res.status(200).json(productos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/:pid", async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const productoBuscado = await productManager.getProductById(pid);
+        res.status(200).json(productoBuscado);
+    } catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        const producto = await productManager.addProduct(req.body);
+        res.status(200).json(producto);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.put("/:pid", async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const prodAct = await productManager.update(req.body, pid);
+        res.status(200).json(prodAct);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-
-    
 });
+
+
+router.delete("/:pid", async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const prodEliminar = await productManager.delete(pid);
+        res.status(200).json({ message: `el producto con id: ${prodEliminar.id} ha sido eliminado` });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 
 export default router;
